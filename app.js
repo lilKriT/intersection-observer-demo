6f1c5136ceee52458dd37719cc98ba5f4880def7
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-15 20:19:35 +0100

    Example completed.

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const sectionOne = document.querySelector("#one");
+const sectionOne = document.querySelector(".one");
+const sections = document.querySelectorAll("section");
 
 const options = {
   root: null, // this means the viewport
-  threshold: 0, // 0 to 1
-  rootMargin: "-150px",
+  threshold: 0.25, // 0 to 1
+  rootMargin: "0px",
 };
 
 // Requires callback and options
 // Callback requires entries and observer
 const observer = new IntersectionObserver(function (entries, observer) {
   entries.forEach((entry) => {
-    console.log(entry.target);
+    // console.log(entry.target);
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.toggle("inverse");
+    observer.unobserve(entry.target);
   });
 }, options);
 
 // Making it work
-observer.observe(sectionOne);
+// observer.observe(sectionOne);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
